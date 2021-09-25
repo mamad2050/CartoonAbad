14dae351574a8150f4b6319a64a9ecda8937a74d
mwdch <m.charoosai1380@gmail.com>
2021-09-25 23:03:14 +0330

    Add NavigationDrawer and NavigationComponent

--- app/src/main/java/com/example/cartoonabad/MainActivity.kt
+++ app/src/main/java/com/example/cartoonabad/MainActivity.kt
@@ -2,10 +2,65 @@ package com.example.cartoonabad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var listener: NavController.OnDestinationChangedListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        navigationView.setupWithNavController(navController)
+
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        listener =
+            NavController.OnDestinationChangedListener { controller, destination, arguments ->
+                when (destination.id) {
+                    R.id.homeFragment -> {
+                        Toast.makeText(this, "homeFragment", Toast.LENGTH_SHORT).show()
+                    }
+                    R.id.favoriteFragment -> {
+                        Toast.makeText(this, "favoriteFragment", Toast.LENGTH_SHORT).show()
+
+                    }
+                    R.id.downloadedFragment -> {
+                        Toast.makeText(this, "downloadedFragment", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navController.addOnDestinationChangedListener(listener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navController.removeOnDestinationChangedListener(listener)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = navHostFragment.navController
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
