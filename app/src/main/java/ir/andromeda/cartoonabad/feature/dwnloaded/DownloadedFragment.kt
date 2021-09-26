d4f083b17052283b409527fdcaf8944f5333f117
mwdch <m.charoosai1380@gmail.com>
2021-09-26 21:02:07 +0330

    add package

--- app/src/main/java/ir/andromeda/cartoonabad/feature/dwnloaded/DownloadedFragment.kt
+++ app/src/main/java/ir/andromeda/cartoonabad/feature/dwnloaded/DownloadedFragment.kt
@@ -1,10 +1,11 @@
-package ir.andromeda.cartoonabad
+package ir.andromeda.cartoonabad.feature.dwnloaded
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ir.andromeda.cartoonabad.R
 
 class DownloadedFragment : Fragment() {
 
