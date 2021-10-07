924e8e96e8be0e3048c3fd6ce9b983417fa7c2cd
matin <mamadzn2050@gmail.com>
2021-10-08 01:32:02 +0330

    init splash screen activity

---
+++ app/src/main/java/ir/andromeda/cartoonabad/feature/splash/SplashActivity.kt
@@ -0,0 +1,29 @@
+package ir.andromeda.cartoonabad.feature.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.WindowManager
+import ir.andromeda.cartoonabad.R
+import ir.andromeda.cartoonabad.feature.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash)
+
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+}
